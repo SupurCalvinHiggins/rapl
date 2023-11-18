0d2d98c4a24a8c18a3e3e4295ef8d5e628c9a839
Calvin Higgins <calvin_higgins2@uri.edu>
2023-11-18 15:08:36 -0500

    Bug fixes.

--- hello.c
+++ hello.c
@@ -10,7 +10,7 @@ static inline uint64_t read_msr(uint32_t msr) {
     uint32_t low, high;
     asm volatile (
         "rdmsr"
-        : "=a" (low), "=d" (high),
+        : "=a" (low), "=d" (high)
         : "c" (msr)
     );
     return ((uint64_t)high << 32) | low;
@@ -23,7 +23,7 @@ static inline bool has_energy_counter() {
         : "=a" (reg[0]), "=b" (reg[1]), "=c" (reg[2]), "=d" (reg[3])
         : "a" (0x80000007) // Section 17.5 AMD64 arch programmer's manual volume 2.
                            // bit 12 of EDX is flag indicating energy facilities
-    )
+    );
     return reg[4] & (1 << 12); 
 }
 
@@ -31,7 +31,7 @@ static ssize_t khello_show(struct kobject *kobj, struct kobj_attribute *attr, ch
 {
     // uint64_t ret = read_msr();
     uint64_t ret = has_energy_counter();
-    return scnprintf(buf, PAGE_SIZE, "%#010x\n", ret);
+    return scnprintf(buf, PAGE_SIZE, "%#010llx\n", ret);
     // return scnprintf(buf, PAGE_SIZE, "Hello world!\n");
 }
 
