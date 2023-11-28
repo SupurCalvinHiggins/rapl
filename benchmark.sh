67520f9a0a799fb3cfb4d67e56a3324563792ead
Carl Stoker <40079934+carlstoker@users.noreply.github.com>
2023-11-28 16:52:48 -0500

    Use journalctl instead of dmesg. Outputs csvs for calibrate, time, sleep, sysbench, and full

--- benchmark.sh
+++ benchmark.sh
@@ -5,8 +5,8 @@ SYSBENCH_TIME=60 # Seconds
 SLEEP_TIME=60 # Seconds
 
 # Array to hold start/end times for time, calibrate, and sysbench
-declare -a start_times
-declare -a end_times
+declare -A start_times
+declare -A end_times
 
 # Gets the output, start time, end time, and dmesg output of the program in the first parameter
 # $1 - The program to run
@@ -15,8 +15,9 @@ declare -a end_times
 function benchmark {
     echo "Running '$1' and writing output to '$2'"
 
-    # Get art time in seconds
+    # Get start time in seconds and YYYY-MM-DD HH:MM:SS
     start_time=$(awk '{print $1}' /proc/uptime)
+    start_time_formatted=$(date +"%Y-%m-%d %H:%M:%S")
 
     # Write the start time in seconds to a file
     echo "Start: $start_time" > "$3/$2.txt"
@@ -24,20 +25,21 @@ function benchmark {
     # Run the program and write its output to a file
     $1 >> "$3/$2.txt" 3>&1
 
-    # Get the end time in seconds
+    # Get the end time in seconds and YYYY-MM-DD HH:MM:SS
     end_time=$(awk '{print $1}' /proc/uptime)
+    end_time_formatted=$(date +"%Y-%m-%d %H:%M:%S")
 
     # Write the end time in seconds to a file
     echo "End: $end_time" >> "$3/$2.txt"
     echo "" >> "$3/$2.txt"
 
     # Store the start and end times in the arrays
-    start_times[$2]=$start_time
-    end_times[$2]=$end_time
+    start_times[$2]=$start_time_formatted
+    end_times[$2]=$end_time_formatted
 
     # Wait one second for dmesg to catch up and then write dmesg output to the file "$2/dmesg_$1"
     sleep 1
-    dmesg -l info | grep '[RCAL]' >> "$3/dmesg_$2.txt"
+    journalctl --boot --grep="\[RCAL\]" --since="$start_time_formatted" --until="$end_time_formatted" --output=short-monotonic > "$3/dmesg_$2.txt"
 }
 
 # Make sure we're running as root
@@ -57,7 +59,6 @@ fi
 
 echo -ne "Beginning make"
 make all --quiet
-# Echo "...Finished" on the same line
 echo -ne "...Finished\n"
 
 insmod rcal.ko
@@ -70,40 +71,38 @@ echo "Sleeping for $SLEEP_TIME seconds..."
 sleep $SLEEP_TIME
 benchmark "sysbench --threads=$SYSBENCH_THREADS --time=$SYSBENCH_TIME --rand-seed=20 threads run" "sysbench" "$RESULTS_DIR"
 
-# Consolidate dmesg output
-cat "$RESULTS_DIR"/dmesg_*.txt > "$RESULTS_DIR/dmesg.txt"
-rm "$RESULTS_DIR"/dmesg_*.txt
+# Create dmesg_full.txt which runs from calibrate's start time to sysbench's end time
+echo "Creating dmesg_full.txt which runs from calibrate's start time (${start_times[calibrate]}) to sysbench's end time (${end_times[sysbench]})..."
+journalctl --boot --grep="\[RCAL\]" --since="${start_times[calibrate]}" --until="${end_times[sysbench]}" --output=short-monotonic > "$RESULTS_DIR/dmesg_full.txt"
 
-# Sort the dmesg output by time and remove duplicates
-sort -n "$RESULTS_DIR/dmesg.txt" | uniq > "$RESULTS_DIR/dmesg_sorted.txt"
+# Create dmesg_sleep.txt which runs from time's end time to sysbench's start time
+echo "Creating dmesg_sleep.txt which runs from time's end time (${end_times[calibrate]}) to sysbench's start time (${start_times[sysbench]})..."
+journalctl --boot --grep="\[RCAL\]" --since="${end_times[time]}" --until="${start_times[sysbench]}" --output=short-monotonic > "$RESULTS_DIR/dmesg_sleep.txt"
 
-# Make the file comma delimited using sed
-# File is in the format [77985.126714] [RCAL] {"start": 1686323097, "end": 1686324186, "count": 15811}
+# Make the files comma delimited using sed
+# Files are in the format [77985.126714] debian kernel: [RCAL] {"start": 1686323097, "end": 1686324186, "count": 15811}
 # We want to convert it to 77985.126714,1686323097,1686324186,15811
-sed -r -i 's/\[([0-9]+\.[0-9]+)\] \[RCAL\] \{\"start\": ([0-9]+), \"end\": ([0-9]+), \"count\": ([0-9]+)\}/\1,\2,\3,\4/g' $RESULTS_DIR/dmesg_sorted.txt
-mv $RESULTS_DIR/dmesg_sorted.txt $RESULTS_DIR/dmesg_full.csv
+echo "Comma delimiting files..."
+sed -r -i 's/\[([0-9]+\.[0-9]+)\]  debian kernel: \[RCAL\] \{\"start\": ([0-9]+), \"end\": ([0-9]+), \"count\": ([0-9]+)\}/\1,\2,\3,\4/g' "$RESULTS_DIR"/dmesg_*.txt
 
-# Add a column for the difference between end and start in place
-awk -F, '{print $1","$2","$3","$4","$3-$2}' OFS=, "$RESULTS_DIR/dmesg_full.csv" > "$RESULTS_DIR/dmesg_full.csv.tmp"
-mv "$RESULTS_DIR/dmesg_full.csv.tmp" "$RESULTS_DIR/dmesg_full.csv"
-
-# Create dmesg_time.csv, dmesg_calibrate.csv, and dmesg_sysbench.csv
-# These files are the same as dmesg_full.csv but only contain the data for the corresponding benchmark
-# based on the start and end times
-awk -F, -v start=${start_times[time]} -v end=${end_times[time]} '{if ($1 >= start && $1 <= end) print $0}' OFS=, "$RESULTS_DIR/dmesg_full.csv" > "$RESULTS_DIR/dmesg_time.csv"
-awk -F, -v start=${start_times[calibrate]} -v end=${end_times[calibrate]} '{if ($1 >= start && $1 <= end) print $0}' OFS=, "$RESULTS_DIR/dmesg_full.csv" > "$RESULTS_DIR/dmesg_calibrate.csv"
-awk -F, -v start=${start_times[sysbench]} -v end=${end_times[sysbench]} '{if ($1 >= start && $1 <= end) print $0}' OFS=, "$RESULTS_DIR/dmesg_full.csv" > "$RESULTS_DIR/dmesg_sysbench.csv"
-
-# Create dmesg_sleep.csv which runs from the end of the time benchmark to the start of the sysbench benchmark
-awk -F, -v start=${end_times[time]} -v end=${start_times[sysbench]} '{if ($1 >= start && $1 <= end) print $0}' OFS=, "$RESULTS_DIR/dmesg_full.csv" > "$RESULTS_DIR/dmesg_sleep.csv"
+# Move the dmest_*.txt files to dmesg_*.csv
+echo "Renaming files to .csv..."
+for file in "$RESULTS_DIR"/dmesg_*.txt; do
+    mv -- "$file" "${file%.txt}.csv"
+done
 
 # Add headers for time,start,end,count,diff
+echo "Adding headers to files..."
 sed -i '1s/^/time,start,end,count,diff\n/' "$RESULTS_DIR"/dmesg_*.csv
 
+# Add diff column to all files
+echo "Adding diff column to files..."
+for file in "$RESULTS_DIR"/dmesg_*.csv; do
+    awk -F, '{if (NR==1) print $0; else print $0","$3-$2}' OFS=, "$file" > "$file.tmp"
+    mv "$file.tmp" "$file"
+done
 
 # Move time* and calibrate* directories to the results directory
+echo "Moving time* and calibrate* directories to the results directory..."
 mv time* "$RESULTS_DIR"
 mv calibrate* "$RESULTS_DIR"
-
-# Clean up
-rm "$RESULTS_DIR/dmesg.txt"
